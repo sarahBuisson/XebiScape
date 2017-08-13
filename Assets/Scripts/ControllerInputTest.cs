8f500d68a33e2f0b07f69cf3aa12eab4525748e6
sbuisson <sbuisson@xebia.fr>
2017-08-14 01:53:50 +0200

    sample of project

---
+++ Assets/Scripts/ControllerInputTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using VRTK;
+public class ControllerInputTest : MonoBehaviour {
+	// Use this for initialization
+	void Start ()
+	{
+		if(GetComponent<VRTK_ControllerEvents>() == null)
+		{
+			Debug.LogError("Need to have VRTK_ControllerEvents component attached to the controller");
+			return;
+		}
+		GetComponent<VRTK_ControllerEvents>().AliasMenuOn += new ControllerInteractionEventHandler(TestInput);
+	}
+	private void TestInput(object sender, ControllerInteractionEventArgs e)
+	{
+		Debug.Log("Seems to be working!");
+	}
+}
